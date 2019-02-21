ee6d5ea4014b921f7b418684e3ca74056186a1b5
mehdinemo <37899528+mehdinemo@users.noreply.github.com>
2019-02-21 12:09:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pyodbc
 
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=servser_name;DATABASE=table_name;UID=id;PWD=pass')
-
 cursor = conn.cursor()
 cursor.execute('SELECT TOP 10 word,user FROM [dbo].[table_name]')
 rows = cursor.fetchall()
 
 f = open('rows.txt',mode='w',encoding='utf-8')
-
 for row in rows:
     f.write(row[0]+'\t'+str(row[1])+'\n')
 f.close()
