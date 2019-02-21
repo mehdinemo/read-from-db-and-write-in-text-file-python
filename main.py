6a7a0c780f234a8946da3e9176b78f2eea40caf0
mehdinemo <37899528+mehdinemo@users.noreply.github.com>
2019-02-21 11:55:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyodbc
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=servser_name;DATABASE=table_name;UID=id;PWD=pass')
 
 cursor = conn.cursor()
-cursor.execute('SELECT TOP 10 word,usersCount FROM [dbo].[Twitter_GroupedWords]')
+cursor.execute('SELECT TOP 10 word,user FROM [dbo].[table_name]')
 rows = cursor.fetchall()
 
 f = open('rows.txt',mode='w',encoding='utf-8')
